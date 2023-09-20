2a657aa0beda3b92812225bfca3ceced5ac83043
dorotajachtoma <jachtoma.do@gmail.com>
2023-09-20 08:29:40 +0200

    integration tests - adding bulk inserting - part II

--- src/test/java/com/djachtoma/configuration/TestSetup.java
+++ src/test/java/com/djachtoma/configuration/TestSetup.java
@@ -1,14 +1,22 @@
 package com.djachtoma.configuration;
 
 import com.djachtoma.Application;
+import com.djachtoma.model.facility.Facility;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static com.djachtoma.util.TestObjectUtil.getFacility;
 
 
 @RunWith(SpringRunner.class)
@@ -22,7 +30,21 @@ public class TestSetup {
 
     @BeforeEach
     public void setup() {
-        
+        List<Facility> facilities = List.of(
+                getFacility(),
+                getFacility(),
+                getFacility());
+        try {
+            this.template.executePipelined(new RedisCallback<Object>() {
+                @Override
+                public Object doInRedis(RedisConnection connection) throws DataAccessException {
+                    facilities.forEach(x -> template.opsForHash().entries(x));
+                    return null;
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/test/java/com/djachtoma/service/FacilityServiceTest.java
+++ src/test/java/com/djachtoma/service/FacilityServiceTest.java
@@ -1,45 +1,26 @@
 package com.djachtoma.service;
 
 import com.djachtoma.exception.FacilityNotFoundException;
-import com.djachtoma.model.constant.Specialization;
-import com.djachtoma.model.constant.Title;
-import com.djachtoma.model.facility.Address;
 import com.djachtoma.model.facility.Facility;
 import com.djachtoma.model.facility.dto.FacilityDTO;
 import com.djachtoma.model.facility.dto.FacilityMapper;
-import com.djachtoma.model.physician.Physician;
-import com.djachtoma.model.physician.dto.PhysicianDTO;
-import com.djachtoma.reference.entity.model.Gender;
-import com.djachtoma.reference.entity.model.IDCard;
-import com.djachtoma.reference.entity.model.PhoneNumber;
 import com.djachtoma.repository.FacilityRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-
-import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.Set;
 
+import static com.djachtoma.util.TestObjectUtil.getFacility;
+import static com.djachtoma.util.TestObjectUtil.getFacilityDTO;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FacilityServiceTest {
-
-    private static final String ID = "ID";
-    private static final String NAME = "NAME";
-    private static final String CITY = "CITY";
-    private static final String ZIP_CODE = "ZIP_CODE";
-    private static final String ADDRESS = "ADDRESS";
-    private static final String SURNAME = "SURNAME";
-    private static final String PHONE_NUMBER = "PHONE_NUMBER";
-    private static final String ID_CARD_SERIES_NUMBER = "ID_CARD_SERIES_NUMBER";
-    private static final LocalDateTime DATE_OF_BIRTH = LocalDateTime.of(2023, 1, 1, 1, 1, 0);
-
 
     @InjectMocks
     private FacilityService facilityService;
@@ -167,67 +148,5 @@ public class FacilityServiceTest {
     }
 
 
-    private static Facility getFacility() {
-        return Facility.builder()
-                .id(ID)
-                .name(NAME)
-                .address(Address.builder()
-                        .city(CITY)
-                        .zipCode(ZIP_CODE)
-                        .address(ADDRESS)
-                        .build())
-                .physicians(Set.of(getPhysician()))
-                .build();
-    }
 
-    private static FacilityDTO getFacilityDTO() {
-        return FacilityDTO.builder()
-                .id(ID)
-                .name(NAME)
-                .city(CITY)
-                .zipCode(ZIP_CODE)
-                .address(ADDRESS)
-                .physicians(Set.of(getPhysicianDTO()))
-                .build();
-    }
-
-    private static PhysicianDTO getPhysicianDTO() {
-        return PhysicianDTO.builder()
-                .id(ID)
-                .name(NAME)
-                .surname(SURNAME)
-                .dateOfBirth(DATE_OF_BIRTH)
-                .idCardSeriesNumber(ID_CARD_SERIES_NUMBER)
-                .gender(Gender.MALE.name())
-                .phoneNumber(PHONE_NUMBER)
-                .address(ADDRESS)
-                .city(CITY)
-                .zipCode(ZIP_CODE)
-                .specializations(Set.of(Specialization.EMERGENCY_MEDICINE.name()))
-                .title(Title.ACF.name())
-                .build();
-    }
-
-    private static Physician getPhysician() {
-        return Physician.builder()
-                .id(ID)
-                .name(NAME)
-                .surname(SURNAME)
-                .dateOfBirth(DATE_OF_BIRTH)
-                .gender(Gender.MALE)
-                .idCard(IDCard.builder()
-                        .seriesNumber(ID_CARD_SERIES_NUMBER)
-                        .build())
-                .phoneNumber(PhoneNumber.builder()
-                        .number(PHONE_NUMBER)
-                        .build())
-                .address(com.djachtoma.reference.entity.model.Address.builder()
-                        .address(ADDRESS)
-                        .city(CITY)
-                        .zipCode(ZIP_CODE)
-                        .build())
-                .specializations(Set.of(Specialization.EMERGENCY_MEDICINE))
-                .title(Title.ACF)
-                .build();
-    }
 }
